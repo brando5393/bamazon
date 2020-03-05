211956014fb1e48cedfcb7b9ed25ca64735d9aea
Brandon Williams <brando5393@gmail.com>
2020-03-04 18:33:19 -0600

    base fles in place

---
+++ bamazon_seeds.sql
@@ -0,0 +1,3 @@
+use bamazon;
+insert into products(product_name,department_name,item_cost,item_quantity)values("sunglasses","apparel",5.00,10)m
+()
---
+++ schema.sql
@@ -0,0 +1,12 @@
+drop database if exists bamazon;
+create database bamazon;
+use bamazon;
+create table products(
+    item_id int not null auto_increment,
+    product_name varchar(255) not null,
+    department_name varchar(255) not null,
+    item_cost decimal(5,2) not null,
+    item_quantity int not null,
+    primary key(item_id);
+
+);
